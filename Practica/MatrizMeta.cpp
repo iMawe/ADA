c6f6ee1798c8ee985cdef12aa74f23b5e7e2c840
Mauricio <wmedinap@ulasalle.edu.pe>
2019-05-08 23:12:56 -0500

    tarea v2

---
+++ Practica/MatrizMeta.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <map>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+	
+	return 0;
+}
--- Practica/Procesos.cpp
+++ Practica/Procesos.cpp
@@ -9,6 +9,8 @@ using namespace std;
 
 int main(){
 	vector< pair < int, string> > M;
+	vector< pair< int, string> > N;
+	int aux = 0;
 
 	string A[] = {"P1", "P2", "P3", "P4", "P5", "P6", "P7", "P8", "P9", "P10"};
 	int B[] = {2, 4, 7, 9, 2, 13, 23, 1, 5, 20};
@@ -20,8 +22,13 @@ int main(){
 	sort(M.begin(), M.end()	);
 
 	for(int i = 0; i < M.size(); i++){
+		aux = aux + M[i].first;
+		M[i].first = aux;
+	}
+
+	for(int i = 0; i < M.size(); i++){
 		
-		cout << M[i].first << ", " << M[i].second << endl;
+		cout << M[i].second << ", " << M[i].first << endl;
 	}
 
 	
